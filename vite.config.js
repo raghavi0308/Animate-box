d5cb5884c564bc4f066d2d9c2755debc7ebcadfa
raghavi0308 <raghavijk3@gmail.com>
2025-06-16 10:54:29 +0530

    index

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,9 @@ export default defineConfig({
     assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
       output: {
         manualChunks: undefined,
         assetFileNames: 'assets/[name]-[hash][extname]',
