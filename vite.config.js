3b1d55c7617c7b17ae2cb690d999b26ae7b6b16a
raghavi0308 <raghavijk3@gmail.com>
2025-06-16 10:59:12 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'src/main.jsx')
       },
       output: {
         manualChunks: undefined,
